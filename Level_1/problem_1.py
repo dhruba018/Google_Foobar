25aa3b616efc441e9f9a3028c749a0df71e7c79f
dhruba018 <dhruba018@gmail.com>
2020-07-12 18:49:14 -0500

    Need formatting

--- Level_1/problem_1.py
+++
@@ -1,9 +0,0 @@
-## Challenge 1.0. Repeatation
-
-## Filter by frequncy count...
-def solution(data, n):
-    freq, data_new = { }, [ ]
-    for k in data:
-        freq[k] = freq[k] + 1 if k in freq else 1
-    data_new = [kk for kk in data if freq[kk] <= n]
-    return data_new
